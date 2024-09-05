5f2b767dda360c38ba4425275b585e6150458252
Matthew <134560399+Get0ra@users.noreply.github.com>
2024-09-05 12:26:59 -0500

    Create Display.java

---
+++ Display.java
@@ -0,0 +1,19 @@
+
+/**
+ * Write a description of class JJ here.
+ *
+ * Matthew 
+ * 29 AUG 2024
+ */
+public class DisplayJava
+{
+    
+public static void main(String[]args)
+{
+String line1 ="      J    A   V    V   A";
+String line2 ="      J   A A   V   V  A A";
+String line3 =" J    J  AAAAA   V V  AAAAA"; 
+String line4 ="  J  J  A     A   V  A     A";
+System.out.printf("%s%n%s%n%s%n%s%n", line1, line2, line3, line4);
+}
+}
