ea2a3179466c54a61d3d881680cb54e7f7c868d8
Matthew <134560399+Get0ra@users.noreply.github.com>
2024-09-05 12:27:34 -0500

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Rectangle here.
+ *
+ * Matthew 
+ * 29 AUG 2024
+ */
+public class Rectangle
+{
+    public static void main(String[] args)
+    {
+        double width = 2.5;
+        double length = 4;
+        double area = 10;
+        double parimeter =13;
+        System.out.printf("Name: Matthew, Lab:2%n");
+        System.out.printf("the area of a rectangle with the length %.1f and width %.1f is: %.2f%n", length, width, area);
+        System.out.printf("the primeter of a rectangle with the length %.1f and width %.1f is: %.2f%n", length, width, parimeter);
+    }
+}
